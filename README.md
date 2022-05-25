e620151e738e8343eb6546390ed4f25cf0fe252c
Alvin <alvin.tol@hotmail.com>
2022-05-24 21:42:43 -0600

    finishes README.md

--- README.md
+++ README.md
@@ -12,21 +12,15 @@ Piggy Financial is an app that aims to help users track their financial goals. W
 ![Break The Piggy Bank](https://github.com/Alvintol/Piggy-Financial/blob/master/react-front-end/public/readmephotos/SavingsPigBreak.png?raw=true)
 ![Budget Page](https://github.com/Alvintol/Piggy-Financial/blob/master/react-front-end/public/readmephotos/BudgetPage.png?raw=true)
 
-## Running the project
-
-You need **TWO** terminal windows/tabs for this (or some other plan for running two Node processes).
-
-In one terminal, `cd` into `react-front-end`. Run `npm install` or `yarn` to install the dependencies. Then run `npm start` or `yarn start`, and go to `localhost:3000` in your browser.
-
-In the other terminal, `cd` into `express-back-end`. Run `npm install` or `yarn` to install the dependencies, then `npm start` or `yarn start` to launch the server.
-
 ## Creating the DB
 
-Use the psql -U development command to login to the PostgreSQL server with the username development and the password development. This command MUST be run in a vagrant terminal, we are using the PostgreSQL installation provided in the vagrant environment.
+Use the `psql -U vagrant` command to login to the PostgreSQL server with the username `vagrant` and the password `123`. This command `MUST` be run in a vagrant terminal, we are using the PostgreSQL installation provided in a vagrant environment.
 
-Create a database with the command CREATE DATABASE piggy;. Run command < \i express-back-end/src/db/schema/create.sql >
+`cd` into `Piggy-Financial` folder. 
+Create a database with the command `CREATE DATABASE piggy;`.\
+Run command `/i express-back-end/src/db/schema/create.sql` 
 
-Copy the .env.example file to .env and fill in the necessary PostgreSQL configuration. The node-postgres library uses these environment variables by default.
+Copy the `.env.example` file to `.env` and fill in the necessary PostgreSQL configuration. The node-postgres library uses these environment variables by default.
 This should look like this below:
 
 ```
@@ -37,6 +31,47 @@ DB_USER=vagrant
 DB_PASS=123
 ```
 
-Run the command < \i express-back-end/src/db/seeds/allinone.sql > to populate the seed data to the tables.
+Run the command `\i express-back-end/src/db/seeds/allinone.sql` to populate the seed data to the tables.
+
+## Running the project
+
+You need **TWO** terminal windows/tabs for this (or some other plan for running two Node processes).
+
+In one terminal, `cd` into `react-front-end`. Run `npm install` or `yarn` to install the dependencies. Then run `npm start` or `yarn start`, and go to `localhost:3000` in your browser.
+
+In the other terminal, `cd` into `express-back-end`. Run `npm install` or `yarn` to install the dependencies, then `npm start` or `yarn start` to launch the server.
+
+Enter app using only the following credentials:
+
+```
+User: 
+  `alvin@hotmail.com`
+or 
+  `ricky@hotmail.com`
+or 
+  `kevin@hotmail.com`
+
+Password: `1`
+```
 
 ## Dependencies
+
+ - `Axios`: Javascript library used to make HTTP requests from node. js or XMLHttpRequests from the browser and it supports the Promise API that is native to JS ES6
+ - `Bcryptjs`: Password encryption in Node.js 
+ - `Chart.js`: JavaScript library for data visualization, which supports eight chart types
+ - `Chartjs`-Adapter-date-fns: Overrides the default date-adapter provided in Chart.js
+ - `Classnames`: A simple utility for conditionally joining classNames together
+ - `Date-fns`: Modern JavaScript date utility library
+ - `Mdb-ui-kit`: Bootstrap 5 UI KIT
+ - `Node-sass`: Sass is a stylesheet language that’s compiled to CSS
+ - `React`: JavaScript library for building user interfaces
+ - `React-Chartjs`-2: React components for Chart.js
+ - `React-Circular`-Progressbar: A circular progress indicator component
+ - `React-Cookie`: Universal cookies for React
+ - `React-Scripts`: Configuration and scripts for Create React App
+
+ ## Current bugs
+
+ - Currency must be picked prior to any other input when entering a new expense if different from previous 
+ - No goal/empty view can be be rendered
+ - New users can be created, but due to above bug, user cannot enter app
--- react-front-end/src/component/mainView/Vacation/Vacation.js
+++ react-front-end/src/component/mainView/Vacation/Vacation.js
@@ -1,74 +1,86 @@
-import React from 'react';
-import ProgressCircle from '../ProgressCircle';
-import '../../../sass/vacation.scss';
+import React from 'react'
+import ProgressCircle from '../ProgressCircle'
+import '../../../sass/vacation.scss'
 import {
   getTotalAmount,
   getDaysTillGoal,
   filteredVacationExpenses,
-  getGoalByID
-} from '../../../helpers/helper_functions';
+  getGoalByID,
+} from '../../../helpers/helper_functions'
 
 export default function Vacation(props) {
-  const vacationInfo = getGoalByID(props.goals, props.userId);
-  const vacationExpenses = filteredVacationExpenses(props.expenses, props.userId, vacationInfo.start_date);
-  const totalSpentOnVacation =
-    props.vactionMode ?
-      (getTotalAmount(vacationExpenses) * props.exchangeRates.rates[props.currentCurrency]).toFixed(2) :
-      getTotalAmount(vacationExpenses);
+  const vacationInfo = getGoalByID(props.goals, props.userId)
+  const vacationExpenses = filteredVacationExpenses(
+    props.expenses,
+    props.userId,
+    vacationInfo.start_date,
+  )
+  const totalSpentOnVacation = props.vacationMode
+    ? (
+        getTotalAmount(vacationExpenses) *
+        props.exchangeRates.rates[props.currentCurrency]
+      ).toFixed(2)
+    : getTotalAmount(vacationExpenses)
   const homeTime = getDaysTillGoal(vacationInfo)
 
-  const dayAllowance =
-    (((vacationInfo.amount - totalSpentOnVacation) / 100 / homeTime) *
-      props.exchangeRates.rates[props.currentCurrency]).toFixed(2);
+  const dayAllowance = (
+    ((vacationInfo.amount - totalSpentOnVacation) / 100 / homeTime) *
+    props.exchangeRates.rates[props.currentCurrency]
+  ).toFixed(2)
 
-  const weekAllowance =
-    (((vacationInfo.amount - totalSpentOnVacation) * 7 / 100 / homeTime) *
-      props.exchangeRates.rates[props.currentCurrency]).toFixed(2);
+  const weekAllowance = (
+    (((vacationInfo.amount - totalSpentOnVacation) * 7) / 100 / homeTime) *
+    props.exchangeRates.rates[props.currentCurrency]
+  ).toFixed(2)
   return (
     <div>
-      <div className='d-flex align-items-center justify-content-center text-center goalbox'>
-        <div className='goal-container'>
+      <div className="d-flex align-items-center justify-content-center text-center goalbox">
+        <div className="goal-container">
           <br />
           <table>
             <thead>
               <tr>
                 <td>
-                  <h1>
-                    {vacationInfo.goal_name}
-                  </h1>
+                  <h1>{vacationInfo.goal_name}</h1>
                 </td>
               </tr>
               <tr>
                 <td>
                   <h1>
-                    {((totalSpentOnVacation * props.exchangeRates.rates[props.currentCurrency]) / 100).toFixed(2)} {props.currentCurrency}/ {(((vacationInfo.amount * props.exchangeRates.rates[props.currentCurrency]) / 100)).toFixed(2)} {props.currentCurrency}
+                    {(
+                      (totalSpentOnVacation *
+                        props.exchangeRates.rates[props.currentCurrency]) /
+                      100
+                    ).toFixed(2)}{' '}
+                    {props.currentCurrency}/{' '}
+                    {(
+                      (vacationInfo.amount *
+                        props.exchangeRates.rates[props.currentCurrency]) /
+                      100
+                    ).toFixed(2)}{' '}
+                    {props.currentCurrency}
                   </h1>
                 </td>
               </tr>
               <tr>
-                <td>
-                  {homeTime} days until home time
-                </td>
+                <td>{homeTime} days until home time</td>
               </tr>
               <tr>
-                <td>
-                  Advised to spend no more than {dayAllowance}/day
-                </td>
+                <td>Advised to spend no more than {dayAllowance}/day</td>
               </tr>
               <tr>
-                <td>
-                  Advised to spend no more than {weekAllowance}/week
-                </td>
+                <td>Advised to spend no more than {weekAllowance}/week</td>
               </tr>
             </thead>
           </table>
           <br />
           <ProgressCircle
-            key='vacationCircle'
+            key="vacationCircle"
             total_saved={(totalSpentOnVacation / 100).toFixed(2)}
-            goalTotal_cents={(vacationInfo.amount / 100).toFixed(2)} />
+            goalTotal_cents={(vacationInfo.amount / 100).toFixed(2)}
+          />
         </div>
       </div>
     </div>
   )
-};
+}
